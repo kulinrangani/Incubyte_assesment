a5dc0d04b7f16fe5df701db18a9cbd1aabead942
kulinrangani <kulinrangani01@gmail.com>
2022-08-21 16:11:43 +0530

    Created First Test

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,9 @@
 public class StringCalculator {
 
+    public int add(String String_value) {
+        if (String_value.equals("")) {
+            return 0;
+        } else
+            return 0;
+    }
 }
--- src/StringCalculatorShould.java
+++ src/StringCalculatorShould.java
@@ -1,3 +1,19 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class StringCalculatorShould {
-    
+    StringCalculator sc;
+
+    @Before
+    public void initializeResouces() {
+        sc = new StringCalculator();
+    }
+
+    @Test
+    public void returnZeroIfStringIsEmpty() {
+        assertEquals(0, sc.add(""));
+    }
+
 }
